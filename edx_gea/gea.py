2a16851d605b711f6d212e60dedc4ea3262df80b
Nadheem Abdulla <63186421+nadheemabdulla@users.noreply.github.com>
2020-10-04 07:31:20 +0530

    Update gea.py

--- edx_gea/gea.py
+++ edx_gea/gea.py
@@ -156,7 +156,7 @@ class GradeExternalActivityXBlock(XBlock, StudioEditableXBlockMixin):
         Gets the content of a resource
         '''
         resource_content = pkg_resources.resource_string(__name__, resource_path)
-        return unicode(resource_content)
+        return str(resource_content)
 
     def resource_string(self, path):
         '''Handy helper for getting resources from our kit.'''
